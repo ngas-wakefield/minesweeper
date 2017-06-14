e210ad3e1ef1a88e0dd76c271baf9bb2a1c25514
ngas-wakefield <ngaswakefield@gmail.com>
2017-06-14 21:34:29 +1200

    edit

--- minesweeper.js
+++ minesweeper.js
@@ -1,9 +1,34 @@
 document.addEventListener('DOMContentLoaded', startGame)
 
 // Define your `board` object here!
-var board = 
+var board = { cells:[
+ {row:0,col:0,isMine:true, hidden:true},
+    {row:0,col:1,isMine:false, hidden:true},
+    {row:0,col:2,isMine:false, hidden:true},
+    {row:1,col:0,isMine:false, hidden:true},
+    {row:1,col:1,isMine:false, hidden:true},
+    {row:1,col:2,isMine:false, hidden:true},
+    {row:2,col:0,isMine:false, hidden:true},
+    {row:2,col:1,isMine:false, hidden:true},
+    {row:2,col:2,isMine:true, hidden:true}
 
-function startGame () {
+]
+
+}
+
+function startGame (name) {
+  var message = "You lose!";
+  
+  
+  
+  if (name =="Bomb") {
+  var inTrouble = true;
+  }
+  
+  if (inTrouble) {
+    console.log(name + "says" + message);
+    
+    }
   // Don't remove this function call: it makes the game work!
   lib.initBoard()
 }
@@ -18,6 +43,12 @@ function checkForWin () {
   // detected that they've won, that is!)
   //   lib.displayMessage('You win!')
 }
+
+
+
+
+
+
 
 // Define this function to count the number of mines around the cell
 // (there could be as many as 8). You don't have to get the surrounding
